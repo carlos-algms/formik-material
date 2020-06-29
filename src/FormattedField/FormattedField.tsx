e7fcb357ab7fd8707994b7ed7cd8b9c3c29ef3cd
Carlos Gomes <carlos.algms@gmail.com>
2020-06-29 19:18:27 -0300

    Created a generic FormattedField

---
+++ src/FormattedField/FormattedField.tsx
@@ -0,0 +1,36 @@
+import TextField from '@material-ui/core/TextField';
+import { Field, FieldProps } from 'formik';
+import { nanoid } from 'nanoid/non-secure';
+import React from 'react';
+import NumberFormat, { NumberFormatProps } from 'react-number-format';
+import ErrorLabel from '../ErrorLabel';
+
+const FormattedField = ({
+	name = nanoid(),
+	className,
+	fullWidth = true,
+	...fieldProps
+}: NumberFormatProps & { fullWidth?: boolean }) => {
+	return (
+		<Field name={name}>
+			{({ field, meta: { error, touched } }: FieldProps) => {
+				const isErrorState = Boolean(error && touched);
+
+				return (
+					<div className={className}>
+						<NumberFormat
+							customInput={TextField}
+							{...fieldProps}
+							{...field}
+							fullWidth={fullWidth}
+							error={isErrorState}
+						/>
+						{isErrorState && <ErrorLabel error={error} />}
+					</div>
+				);
+			}}
+		</Field>
+	);
+};
+
+export default FormattedField;
