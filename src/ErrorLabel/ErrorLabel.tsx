badc02f9eedf5a1fa1ac970c51a252eb9ef28f3e
Carlos Gomes <carlos.algms@gmail.com>
2020-06-29 19:42:10 -0300

    Import makeStyles from material-ui core

--- src/ErrorLabel/ErrorLabel.tsx
+++ src/ErrorLabel/ErrorLabel.tsx
@@ -1,15 +1,15 @@
-import { makeStyles } from '@material-ui/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import React from 'react';
 
 const useClasses = makeStyles({
-	error: {
-		color: 'red',
-	},
+  error: {
+    color: 'red',
+  },
 });
 
 const ErrorLabel = ({ error }: { error: any }) => {
-	const classes = useClasses();
-	return <div className={classes.error}>{error}</div>;
+  const classes = useClasses();
+  return <div className={classes.error}>{error}</div>;
 };
 
 export default ErrorLabel;
