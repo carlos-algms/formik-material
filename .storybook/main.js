1e91d16d6f971839cb879f8c601ecf688bfec051
Carlos Gomes <carlos.algms@gmail.com>
2020-06-29 14:23:51 -0300

    Setup storybook with mdx support

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+	stories: ['../src/**/*.stories.@(tsx|mdx)'],
+	addons: [
+		'@storybook/preset-typescript',
+		'@storybook/addon-actions',
+		'@storybook/addon-links',
+		'@storybook/addon-docs',
+	],
+};
