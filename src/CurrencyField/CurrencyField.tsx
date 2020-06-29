e91678fcdcca1d9f3f9c82476610881ba0316482
Carlos Gomes <carlos.algms@gmail.com>
2020-06-29 19:45:18 -0300

    Created a CurrencyField for values with symbol

---
+++ src/CurrencyField/CurrencyField.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import type { NumberFormatProps } from 'react-number-format';
+import NumberField from '../NumberField';
+
+const CurrencyField = (props: NumberFormatProps) => {
+	return <NumberField prefix="R$ " decimalScale={2} fixedDecimalScale type="text" {...props} />;
+};
+
+export default CurrencyField;
