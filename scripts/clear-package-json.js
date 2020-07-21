5d6ad7da9eb186792c1be1e20e8a9466c6902899
Carlos Gomes <carlos.algms@gmail.com>
2020-07-21 13:23:50 +0200

    Adding a script to clear package.json on build

---
+++ scripts/clear-package-json.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+const ENCODING = 'utf-8';
+
+const packageJsonPath = path.join(__dirname, '..', 'package.json');
+
+fs.readFile(packageJsonPath, ENCODING, (readError, content) => {
+  if (readError) {
+    console.error(readError);
+    process.exit(1);
+  }
+
+  const data = JSON.parse(content);
+  const { devDependencies, scripts, ...clearedProps } = data;
+  const newContent = JSON.stringify(clearedProps, null, 2);
+  const distPackageJsonPath = path.join(__dirname, '..', 'dist', 'package.json');
+
+  fs.writeFile(distPackageJsonPath, newContent, ENCODING, (writeError) => {
+    if (writeError) {
+      console.error(writeError);
+      process.exit(1);
+    }
+
+    console.log('Finished writing cleaned "dist/package.json"');
+  });
+});
