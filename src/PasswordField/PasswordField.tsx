2565684c6d182b3082e26fea6c7e30609bac27d9
Carlos Gomes <carlos.algms@gmail.com>
2021-09-23 20:36:03 +0200

    Added the new Password field
    With the visibility toggle

---
+++ src/PasswordField/PasswordField.tsx
@@ -0,0 +1,27 @@
+import IconButton from '@material-ui/core/IconButton';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import { TextFieldProps } from '@material-ui/core/TextField';
+import VisibilityOffOutlinedIcon from '@material-ui/icons/VisibilityOffOutlined';
+import VisibilityOutlinedIcon from '@material-ui/icons/VisibilityOutlined';
+import React, { useState } from 'react';
+import TextField from '../TextField';
+
+export default function PasswordField(props: TextFieldProps) {
+  const [isVisible, setIsVisible] = useState(false);
+
+  return (
+    <TextField
+      {...props}
+      type={isVisible ? 'text' : 'password'}
+      InputProps={{
+        endAdornment: (
+          <InputAdornment position="end">
+            <IconButton onClick={() => setIsVisible(!isVisible)}>
+              {isVisible ? <VisibilityOutlinedIcon /> : <VisibilityOffOutlinedIcon />}
+            </IconButton>
+          </InputAdornment>
+        ),
+      }}
+    />
+  );
+}
