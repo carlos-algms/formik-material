74e3af4c7af61e48dab4dd954cd897182ef775b2
Carlos Gomes <carlos-algms@users.noreply.github.com>
2021-09-29 00:48:01 +0200

    feat(checkbox): Adding the checkbox field (#22)
    * feat(checkbox): Adding the checkbox field
    
    * Setup Jest runner
    
    * Adding tests for the Checkbox field
    
    * fix jest command

---
+++ src/CheckboxField/CheckboxField.test.tsx
@@ -0,0 +1,31 @@
+import { act, fireEvent, render } from '@testing-library/react';
+import React from 'react';
+import Form from '../Form';
+import SubmitButton from '../SubmitButton';
+import { delay } from '../tests/helpers';
+import CheckboxField from './CheckboxField';
+
+test('should render a MUI Checkbox and error label', async () => {
+  const submitMock = jest.fn();
+  const values = { testField: false };
+  const validateMock = jest.fn(() => ({
+    testField: 'Not valid',
+  }));
+
+  const { container, getByRole } = render(
+    <Form onSubmitForm={submitMock} initialValues={values} initialError validate={validateMock}>
+      <CheckboxField name="testField" label="Test Checkbox" className="checkbox-test-class" />
+      <SubmitButton>Send</SubmitButton>
+    </Form>,
+  );
+
+  const submitButton = getByRole('button');
+
+  await act(async () => {
+    fireEvent.click(submitButton);
+    await delay();
+  });
+
+  const wrapper = container.querySelector('.checkbox-test-class');
+  expect(wrapper).toMatchSnapshot();
+});
