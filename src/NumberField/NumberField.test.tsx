ab46c7fbbb63d0aff08ccd3885ef8cb7998c9068
Carlos Gomes <carlos.algms@gmail.com>
2023-04-17 21:22:39 +0200

    Adding tests for the Number Field

--- src/NumberField/NumberField.test.tsx
+++ src/NumberField/NumberField.test.tsx
@@ -1,3 +1,28 @@
-test('should set the value as number', () => {
-  expect(true).toBeFalsy();
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import FormDemo from '../shared/FormDemo';
+import NumberField from './NumberField';
+
+test('should set the value as number', async () => {
+  const handler = jest.fn();
+
+  const { container } = render(
+    <FormDemo initialValues={{ age: '0' }} onSubmit={handler}>
+      <NumberField name="age" data-testid="age" />
+    </FormDemo>,
+  );
+
+  const field = container.querySelector('input') as HTMLInputElement;
+  expect(field).toBeInTheDocument();
+
+  const user = userEvent.setup();
+  await user.type(field, '30');
+  await user.click(screen.getByRole('button'));
+
+  await waitFor(() => {
+    const [values] = handler.mock.lastCall;
+    return expect(values).toStrictEqual({
+      age: 30,
+    });
+  });
 });
